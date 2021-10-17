07a924a49827fb62ad110cde9feb0aad57616cae
Lee Siang Yin <siangyin916@gmail.com>
2021-10-17 15:51:38 +0800

    testing to publish shoppingcart build page

--- ShoppingCart/build/service-worker.js
+++ ShoppingCart/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactMiniProj/ShoppingCart/precache-manifest.49953d176145ba6c535860a29043caf3.js"
+  "./precache-manifest.fa3aa4279818ddff137f2655a53ea708.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactMiniProj/ShoppingCart/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
