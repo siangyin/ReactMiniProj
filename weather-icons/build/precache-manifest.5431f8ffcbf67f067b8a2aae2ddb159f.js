985e7237f88e9cece1c12a5cc00d23c4c17e27f0
Lee Siang Yin <siangyin916@gmail.com>
2021-10-17 22:59:32 +0800

    weather-icon files update

---
+++ weather-icons/build/precache-manifest.5431f8ffcbf67f067b8a2aae2ddb159f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5106247257c7d4663d40a1f599993e71",
+    "url": "./index.html"
+  },
+  {
+    "revision": "20a02a118475a2724552",
+    "url": "./static/css/main.f35f9068.chunk.css"
+  },
+  {
+    "revision": "bd037e50a1890bc0e48b",
+    "url": "./static/js/2.2d770aba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.2d770aba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20a02a118475a2724552",
+    "url": "./static/js/main.1fc4bd5f.chunk.js"
+  },
+  {
+    "revision": "6e02fca4aa038177a05a",
+    "url": "./static/js/runtime-main.86de3626.js"
+  }
+]);
