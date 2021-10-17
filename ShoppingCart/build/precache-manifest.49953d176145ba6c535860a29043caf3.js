8e02a159493716f44191ad40a9315a6f8857a742
Lee Siang Yin <siangyin916@gmail.com>
2021-10-17 09:42:24 +0800

    initialize folder

---
+++ ShoppingCart/build/precache-manifest.49953d176145ba6c535860a29043caf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6812b13d1f454acfb5ee95a200e3a287",
+    "url": "/ReactMiniProj/ShoppingCart/index.html"
+  },
+  {
+    "revision": "686d54c02ff19817c3c1",
+    "url": "/ReactMiniProj/ShoppingCart/static/css/main.c506c6ee.chunk.css"
+  },
+  {
+    "revision": "98ad9538443952f13f8f",
+    "url": "/ReactMiniProj/ShoppingCart/static/js/2.eee01206.chunk.js"
+  },
+  {
+    "revision": "686d54c02ff19817c3c1",
+    "url": "/ReactMiniProj/ShoppingCart/static/js/main.1c31f0e3.chunk.js"
+  },
+  {
+    "revision": "b5c495dfa838b0d2c658",
+    "url": "/ReactMiniProj/ShoppingCart/static/js/runtime~main.6ee99e82.js"
+  }
+]);
---
+++ ShoppingCart2/build/precache-manifest.45d26a1f68f38eb8528a49d3536ec4c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1082658a3162da09a0deed8be32cfb43",
+    "url": "/ReactMiniProj/index.html"
+  },
+  {
+    "revision": "f25705f05c2ac8e40b71",
+    "url": "/ReactMiniProj/static/css/main.c506c6ee.chunk.css"
+  },
+  {
+    "revision": "f2477949b597b6440bbf",
+    "url": "/ReactMiniProj/static/js/2.a636507f.chunk.js"
+  },
+  {
+    "revision": "f25705f05c2ac8e40b71",
+    "url": "/ReactMiniProj/static/js/main.3f6e0068.chunk.js"
+  },
+  {
+    "revision": "a032688f6edf14828b37",
+    "url": "/ReactMiniProj/static/js/runtime~main.e3cd14a5.js"
+  }
+]);
