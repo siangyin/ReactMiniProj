c76b4886fe775c9f2b4874ae46b0bc84b30949ad
Lee Siang Yin <siangyin916@gmail.com>
2021-10-18 16:04:21 +0800

    completed solution with input

---
+++ shopping-cart/build/precache-manifest.63c3bad76f08309695c195f7337448e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eeef98f4845ce77ffd8fb5356d37d9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de8430ac7b8f1e0b185a",
+    "url": "./static/css/main.74dc94a6.chunk.css"
+  },
+  {
+    "revision": "dec55b46368f9b8f2d4f",
+    "url": "./static/js/2.f05edd3d.chunk.js"
+  },
+  {
+    "revision": "de8430ac7b8f1e0b185a",
+    "url": "./static/js/main.cdc5282f.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
--- shopping-cart/build/service-worker.js
+++ shopping-cart/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3cafa0568f6f81e3f4bd865feab0b9cd.js"
+  "./precache-manifest.63c3bad76f08309695c195f7337448e7.js"
 );
 
 self.addEventListener('message', (event) => {
