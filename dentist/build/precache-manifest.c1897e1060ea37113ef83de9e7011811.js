44d6142ab50d80154fb5a030e6d1a60f6e91f10e
Lee Siang Yin <siangyin916@gmail.com>
2021-10-23 13:48:21 +0800

    updated json

---
+++ dentist/build/precache-manifest.c1897e1060ea37113ef83de9e7011811.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f247cdb3892f3217cd51d45f0cc6983",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2991fc505783798caf27",
+    "url": "./static/css/main.a704f12f.chunk.css"
+  },
+  {
+    "revision": "babe73adec65d6932559",
+    "url": "./static/js/2.cea732da.chunk.js"
+  },
+  {
+    "revision": "2991fc505783798caf27",
+    "url": "./static/js/main.2baac307.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
