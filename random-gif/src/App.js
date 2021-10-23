3a5172f94a971050975cab937a1e57a3493a3007
Lee Siang Yin <siangyin916@gmail.com>
2021-10-23 18:25:01 +0800

    created random-gif react app

---
+++ random-gif/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<h2>Random Gif Generator</h2>
+			</header>
+		</div>
+	);
+}
+
+export default App;
